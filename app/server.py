c143ebe411ed3324e2a0c32c2bb744c31a4f2dcd
zoupeng8299 <zoupeng8299@126.com>
2025-01-18 21:42:25 +0800

    update to my local IP

--- app/server.py
+++ app/server.py
@@ -14,4 +14,4 @@ add_routes(app, CustomLLM())
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="192.168.1.100", port=11434)
--- test_server.py
+++ test_server.py
@@ -1,8 +1,8 @@
 from langserve import RemoteRunnable
 
-chat_model = RemoteRunnable("http://localhost:8080/chat")
+chat_model = RemoteRunnable("http://192.168.1.100:11434/chat")
 
 print(f"Pinging chat model: {chat_model.invoke('help').content}")
 
-instruct_model = RemoteRunnable("http://localhost:8080")
+instruct_model = RemoteRunnable("http://192.168.1.100:11434")
 print(f"Pinging instruct model: {instruct_model.invoke('help')}")
